cdc64623be16d595a20ce7b9f6bc35d0a40d4a3b
Aitor Gómez Goiri <aitor.gomez@deusto.es>
2014-07-10 12:40:55 +0200

    Updating data with actual memory allocated

--- evaluation/analysis/compare_memory.py
+++ evaluation/analysis/compare_memory.py
@@ -27,17 +27,23 @@ class DiagramGenerator:
     def __init__(self, title, data):
         
         # http://colorschemedesigner.com/previous/colorscheme2/index-es.html?tetrad;100;0;225;0.3;-0.8;0.3;0.5;0.1;0.9;0.5;0.75;0.3;-0.8;0.3;0.5;0.1;0.9;0.5;0.75;0.3;-0.8;0.3;0.5;0.1;0.9;0.5;0.75;0.3;-0.8;0.3;0.5;0.1;0.9;0.5;0.75;0
-        self.linesColors = ("#E6AC73", "#CFE673", "#507EA1", "#E67373", "#8A458A")
+        self.linesColors = ("#2E8199", "#73CCE6", "#996735", "#E6AC73")
         # self.linesShapes = ('xk-','+k-.','Dk--') # avoiding spaghetti lines
         self.ci = ChartImprover( title = None, # title,
-                                 xlabel = 'Key lengths',
-                                 ylabel = {"label": 'Sets of fields able to manage', "x": -0.02, "y": 1.1},
+                                 xlabel = 'Key length (bits)',
+                                 ylabel = {"label": '# of information sets', "x": -0.02, "y": 1.1},
                                  legend_from_to = (0.04, 1.0) )
-        self.generate(data)
+	
+	fig = plt.figure(figsize=(18,6))
+        ax1 = fig.add_subplot(1,2,1)
+        ax2 = fig.add_subplot(1,2,2)
+        
+        self.generate(ax1, data["uno"])
+        self.generate(ax2, data["mega"])
             
-    def generate(self, data):
-	fig = plt.figure(figsize=(10, 6))
-        ax = fig.add_subplot(1,1,1)
+    def generate(self, ax, data):
+	#fig = plt.figure(figsize=(10, 6))
+        #ax = fig.add_subplot(1,1,1)
         
         devices_names = data.keys()
         groups_names = ["128", "256", "512"]
@@ -75,12 +81,14 @@ class DiagramGenerator:
                 bottom = caches_avg
         )
         
+        plt.sca( ax )
         plt.xticks( [i+width for i in ind], groups_names)
         
         ax.set_xlim(0.5, len(groups_names)+1)
         ax.set_ylim(0)
         
         self.ci.improve_following_guidelines(ax)
+        plt.ylim((0,480))
     
     def show(self):
         plt.show()
@@ -95,11 +103,19 @@ if __name__ == '__main__':
                     help='Specify the folder containing the result files to parse.')
     args = argp.parse_args()
     
-    data = {"simple" : {}, "cachingKeys": {}}
-    data["simple"]["sram"] = (2, 2, 2)
-    data["simple"]["eeprom"] = (8, 3, 1)
-    data["cachingKeys"]["sram"] = (1, 1, 1)
-    data["cachingKeys"]["eeprom"] = (5, 2, 1)
+    data = {"uno": {"simple" : {}, "cachingKeys": {}}, "mega": {"simple" : {}, "cachingKeys": {}}}
+    
+    # Arduino Uno
+    data["uno"]["simple"]["sram"] = (26, 24, 20)
+    data["uno"]["simple"]["eeprom"] = (64, 64, 64)
+    data["uno"]["cachingKeys"]["sram"] = (13, 8, 5)
+    data["uno"]["cachingKeys"]["eeprom"] = (20, 12, 7)
+    
+    # Arduino Mega
+    data["mega"]["simple"]["sram"] = (191, 189, 186)
+    data["mega"]["simple"]["eeprom"] = (256, 256, 256)
+    data["mega"]["cachingKeys"]["sram"] = (92, 63, 38)
+    data["mega"]["cachingKeys"]["eeprom"] = (81, 49, 28)
     
     d = DiagramGenerator("Memory needed", data)
-    d.save('/tmp/memory_kdfs.pdf')
+    d.save('/tmp/memory-eval.svg')
